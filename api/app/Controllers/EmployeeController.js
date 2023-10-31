a188c8d5acccc7ad7a4c05c0bcc64e9047f3dc51
Alex Hicks <89438002+hicksale@users.noreply.github.com>
2023-10-31 11:05:24 -0700

    Update EmployeeController.js
    Added allEmployees and employeeWithID

--- api/app/Controllers/EmployeeController.js
+++ api/app/Controllers/EmployeeController.js
@@ -1,1 +1,66 @@
+const dbConnection = require('../../database/mySQLconnect');
 
+const allEmployees = async (ctx) => {
+    console.log('routes all routes called.');
+    return new Promise((resolve, reject) => {
+        const query = `
+                       SELECT *
+                        FROM 
+                            employee
+                        ORDER BY employeeID
+                        `;
+        dbConnection.query({
+            sql: query,
+        }, (error, tuples) => {
+            if (error) {
+                console.log("Connection error in EmployeeController::allEmployees", error);
+                return reject(error);
+            }
+            ctx.body = tuples;
+            ctx.status = 200;
+            return resolve();
+        });
+    }).catch(err => {
+        console.log("Database connection error in allEmployees.", err);
+        // The UI side will have to look for the value of status and
+        // if it is not 200, act appropriately.
+        ctx.body = [];
+        ctx.status = 500;
+    });
+}
+
+const employeeWithID = (ctx) => {
+    return new Promise((resolve, reject) => {
+        const query = `
+                       SELECT *
+                        FROM 
+                            employee
+                        WHERE
+                            employeeID = ?
+                        ORDER BY employeeID
+                        `;
+        dbConnection.query({
+            sql: query,
+            values: [ctx.params.employeeID]
+        }, (error, tuples) => {
+            if (error) {
+                console.log("Connection error in EmployeeController::employeeWithID", error);
+                return reject(error);
+            }
+            ctx.body = tuples;
+            ctx.status = 200;
+            return resolve();
+        });
+    }).catch(err => {
+        console.log("Database connection error in employeeWithID.", err);
+        // The UI side will have to look for the value of status and
+        // if it is not 200, act appropriately.
+        ctx.body = [];
+        ctx.status = 500;
+    });
+}
+
+module.exports = {
+    allEmployees,
+    employeeWithID
+}
