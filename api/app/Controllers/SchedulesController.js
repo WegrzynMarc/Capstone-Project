1cd5580023f4af77ae71eadec397e4f512d52133
Alex Hicks <89438002+hicksale@users.noreply.github.com>
2023-10-31 11:11:00 -0700

    Create SchedulesController.js
    Skeleton

---
+++ api/app/Controllers/SchedulesController.js
@@ -0,0 +1,15 @@
+const dbConnection = require('../../database/mySQLconnect');
+
+// Check a specific employee's schedule
+// Check a specific employee's specific schedule
+
+module.exports = {
+    employeeSchedule,
+    employeeMonday,
+    employeeTuesday,
+    employeeWednesday,
+    employeeThursday,
+    employeeFriday,
+    employeeSaturday,
+    employeeSunday
+}
