067af13353f08c152893ce1aa8058eaff37f9074
Alex Hicks <89438002+hicksale@users.noreply.github.com>
2023-11-07 12:22:01 -0800

    Create MessageController.js

---
+++ api/app/Controllers/MessageController.js
@@ -0,0 +1,136 @@
+const dbConnection = require('../../database/mySQLconnect');
+
+const allMessages = async (ctx) => {
+    console.log('messages all messages called.');
+    return new Promise((resolve, reject) => {
+        const query = `
+                       SELECT *
+                        FROM 
+                            messages
+                        ORDER BY messageID
+                        `;
+        dbConnection.query({
+            sql: query,
+        }, (error, tuples) => {
+            if (error) {
+                console.log("Connection error in MessageController::allMessages", error);
+                return reject(error);
+            }
+            ctx.body = tuples;
+            ctx.status = 200;
+            return resolve();
+        });
+    }).catch(err => {
+        console.log("Database connection error in allMessages.", err);
+        // The UI side will have to look for the value of status and
+        // if it is not 200, act appropriately.
+        ctx.body = [];
+        ctx.status = 500;
+    });
+}
+
+const messageWithMessageID = (ctx) => {
+        return new Promise((resolve, reject) => {
+            const query = `
+                       SELECT *
+                        FROM 
+                            messages
+                        WHERE 
+                            messageID = ?
+                        ORDER BY messageID
+                        `;
+            dbConnection.query({
+                sql: query,
+                values: [ctx.params.messageID]
+            }, (error, tuples) => {
+                if (error) {
+                    console.log("Connection error in MessageController::messageWithMessageID", error);
+                    ctx.body = [];
+                    ctx.status = 200;
+                    return reject(error);
+                }
+                ctx.body = tuples;
+                ctx.status = 200;
+                return resolve();
+            });
+        }).catch(err => {
+            console.log("Database connection error in messageWithMessageID.", err);
+            // The UI side will have to look for the value of status and
+            // if it is not 200, act appropriately.
+            ctx.body = [];
+            ctx.status = 500;
+        });
+}
+
+const messagesSent = (ctx) => {
+        return new Promise((resolve, reject) => {
+            const query = `
+                       SELECT *
+                        FROM 
+                            messages
+                        WHERE 
+                            senderID = ?
+                        ORDER BY messageID
+                        `;
+            dbConnection.query({
+                sql: query,
+                values: [ctx.params.senderID]
+            }, (error, tuples) => {
+                if (error) {
+                    console.log("Connection error in MessageController::messagesSent", error);
+                    ctx.body = [];
+                    ctx.status = 200;
+                    return reject(error);
+                }
+                ctx.body = tuples;
+                ctx.status = 200;
+                return resolve();
+            });
+        }).catch(err => {
+            console.log("Database connection error in messagesSent.", err);
+            // The UI side will have to look for the value of status and
+            // if it is not 200, act appropriately.
+            ctx.body = [];
+            ctx.status = 500;
+        });
+}
+
+const messagesReceived = (ctx) => {
+        return new Promise((resolve, reject) => {
+            const query = `
+                       SELECT *
+                        FROM 
+                            messages
+                        WHERE 
+                            receiverID = ?
+                        ORDER BY messageID
+                        `;
+            dbConnection.query({
+                sql: query,
+                values: [ctx.params.receiverID]
+            }, (error, tuples) => {
+                if (error) {
+                    console.log("Connection error in MessageController::messagesReceived", error);
+                    ctx.body = [];
+                    ctx.status = 200;
+                    return reject(error);
+                }
+                ctx.body = tuples;
+                ctx.status = 200;
+                return resolve();
+            });
+        }).catch(err => {
+            console.log("Database connection error in messagesReceived.", err);
+            // The UI side will have to look for the value of status and
+            // if it is not 200, act appropriately.
+            ctx.body = [];
+            ctx.status = 500;
+        });
+}
+
+module.exports = {
+      allMessages,
+      messageWithMessageID,
+      messagesSent,
+      messagesReceived
+}
