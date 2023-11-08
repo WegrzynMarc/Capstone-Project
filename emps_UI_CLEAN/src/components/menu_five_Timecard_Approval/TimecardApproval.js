e64f1afdcf53d71e6570f105fdc355e866e4706a
WegrzynMarc <148284234+WegrzynMarc@users.noreply.github.com>
2023-11-07 16:29:08 -0800

    Add files via upload

---
+++ emps_UI_CLEAN/src/components/menu_five_Timecard_Approval/TimecardApproval.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function TimecardApproval(props) {
+    return (
+        <Fragment>
+            <Typography component="div" variant='h3'>
+                Timecard Hour Approval Here
+            </Typography>
+        </Fragment>
+    )
+}
