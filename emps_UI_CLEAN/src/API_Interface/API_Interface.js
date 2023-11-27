82571b624767b7e863b46adcc588edc029b7f712
WegrzynMarc <148284234+WegrzynMarc@users.noreply.github.com>
2023-11-27 12:28:45 -0800

    Add files via upload

---
+++ emps_UI_CLEAN/src/API_Interface/API_Interface.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+const AxiosConfigured = () => {
+    // Indicate to the API that all requests for this app are AJAX
+    axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+    // Set the baseURL for all requests to the API domain instead of the current domain
+    // axios.defaults.baseURL = `http://localhost:8443/api/v1`;
+    axios.defaults.baseURL = `http://localhost:8443/api/v1`;
+
+
+    // Allow the browser to send cookies to the API domain (which include auth_token)
+    axios.defaults.withCredentials = true;
+
+
+//    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrf_token;
+
+    return axios;
+};
+
+
+const axiosAgent = AxiosConfigured();
+
+export default class APIInterface {
+
+    async getUserInfo(firstName, lastName) {
+        return axiosAgent.get(`login/${firstName}/${lastName}`)
+            .then(userInfo => userInfo.data)
+            .catch(error => (
+                {
+                    error,
+                    employeeID: undefined,
+                    permLevel: undefined
+                 }));
+    }
+
+    async messagesWithEmployeeID(employeeID) {
+        return axiosAgent.get(`message/${employeeID}`);
+    }
+
+    async messagesUpdate(employeeID, messageID, message) {
+        axiosAgent.get(`message/${employeeID}/${messageID}/${message}`);
+    }
+
+
+
+    //Reference API Calls for quick reference
+    //Do not impliment the commented out lines, these are only for reference and not production
+    /*
+    async allRoutes() {
+        return axiosAgent.get(`routes/all-routes`);
+    }
+
+    async routesWithID(routeID) {
+        return axiosAgent.get(`routes/${routeID}`);
+    }
+    */
+}
