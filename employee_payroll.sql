cceac13ddd816f669031d78eebeb48b931b89672
WegrzynMarc <148284234+WegrzynMarc@users.noreply.github.com>
2023-12-01 15:32:14 -0800

    Add files via upload

---
+++ employee_payroll.sql
@@ -0,0 +1,160 @@
+-- MariaDB dump 10.19  Distrib 10.6.12-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: employee_payroll
+-- ------------------------------------------------------
+-- Server version	10.6.12-MariaDB-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employee` (
+  `employeeID` char(20) NOT NULL,
+  `firstName` varchar(20) DEFAULT NULL,
+  `lastName` varchar(20) DEFAULT NULL,
+  `employeeAddress` varchar(100) DEFAULT NULL,
+  `phoneNumber` varchar(12) DEFAULT NULL,
+  `startDate` date DEFAULT NULL,
+  `endDate` date DEFAULT NULL,
+  `isSalaried` tinyint(1) DEFAULT NULL,
+  `salary` decimal(13,2) DEFAULT NULL,
+  `hourlyWage` decimal(5,2) DEFAULT NULL,
+  `hoursWTD` decimal(5,2) DEFAULT NULL,
+  `paid_hours` decimal(5,2) DEFAULT NULL,
+  `unpaid_hours` decimal(5,2) DEFAULT NULL,
+  `isManager` tinyint(1) DEFAULT NULL,
+  `PTO` decimal(5,2) DEFAULT NULL,
+  `clockedIn` tinyint(1) DEFAULT NULL,
+  `timeClockedIn` datetime DEFAULT NULL,
+  `timeClockedOut` datetime DEFAULT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  PRIMARY KEY (`employeeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES 
+('000000', 'System', 'Message', 'NA', '0', '2022-01-01',NULL,0,NULL,0,0,0,0,0,0,0,'',NULL, 'hjklgafsd', 'bvaibivbaretaqhgadfs'),
+('000001','John','Smith','1234 Main Street','15558675309','2022-01-01',NULL,0,NULL,23.00,0.00,0.00,0.49,            0,25.00,0,'2023-11-22 22:15:50',NULL, 'Account1', 'Admin'),
+('000002','Argyle','DeSilva','12 North St.','15553468056','2022-12-21',NULL,0,NULL,16.00,0.00,0.00,0.00,            1,30.00,0,NULL,NULL, 'Manager1', 'Admin'),
+('000003','Beauford','Johnson','122 South Street.','15555342456','2022-03-11',NULL,1,900000.00,NULL,0.00,0.00,0.00, 0,175.00,0,NULL,NULL, 'Account2', 'Admin'),
+('000004','Brett','Lister','573 Chicken Ave.','15555555456','2022-03-21',NULL,1,100000.00,NULL,0.00,0.00,0.00,      0,25.00,0,NULL,NULL, 'Account3', 'Admin'),
+('000005','Esteban','Salamanca','427 West St.','15555551234','2021-02-22',NULL,0,NULL,33.00,0.00,0.00,0.00,         1,25.00,0,NULL,NULL, 'Manager2', 'Admin'),
+('000006','Sam','Fisher','827 NSA St','15552535456','2022-01-01',NULL,1,70000.00,NULL,0.00,0.00,0.00,               0,25.00,0,NULL,NULL, 'Account4', 'Admin'),
+('000007','Nancy','Meyers','67 Left Blvd','15554635674','2022-10-11',NULL,1,78000.00,NULL,0.00,0.00,0.00,           1,126.00,0,NULL,NULL, 'Manager2', 'Admin'),
+('000008','Neil','Hector','12 North St.','15552558056','2022-11-01',NULL,0,NULL,36.00,0.00,0.00,0.00,               0,30.00,0,NULL,NULL, 'Account5', 'Admin'),
+('000009','Samantha','Tudor','1600 C Street','15552565674','2022-12-01',NULL,1,180000.00,NULL,0.00,0.00,            0.00,0,126.00,0,NULL,NULL, 'Account6', 'Admin'),
+('000010','Amber','Stone','2424 G Street','15552567856','2022-01-01',NULL,1,80000.00,NULL,0.00,0.00,0.00,           0,11.00,0,NULL,NULL, 'Account7', 'Admin'),
+('000011','Martin','Law','178 Law Drive.','15555315456','2022-03-21',NULL,1,100000.00,NULL,0.00,0.00,0.00,          0,250.00,0,'2023-11-22 22:16:16',NULL, 'Account8', 'Admin');
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `messages` (
+  `messageID` char(20) NOT NULL,
+  `senderID` char(20) NOT NULL,
+  `receiverID` char(20) NOT NULL,
+  `date` DATE DEFAULT NULL,
+  `hours` char(20) NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`messageID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messages`
+--
+
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+INSERT INTO `messages` VALUES 
+('000000', '000002', '000001', '2023-01-01', 0, "Just to let you know, make sure the doors are locked when you leave."),
+('000001', '000002', '000003', '2023-01-01', 0, "Keep the workplace clean, and remember to throw away any trash you make."),
+('000002', '000000', '000001', '2023-01-01', 12,"Flagged for working beyond scheduled hours"),
+('000003', '000000', '000003', '2023-01-01', 9, "Flagged for working on a day not scheduled"),
+('000004', '000002', '000004', '2023-01-01', 0, "No drinking while on the job. Consider this a warning."),
+('000005', '000002', '000005', '2023-01-01', 0, "This is a complex message designed to show the messaging system works. This text is filler and designed to showcase how the message system breaks things down."),
+('000006', '000002', '000006', '2023-01-01', 0, "Generic Message to show the system works"),
+('000007', '000002', '000007', '2023-01-01', 0, "Generic Message to show the system works"),
+('000008', '000002', '000008', '2023-01-01', 0, "Generic Message to show the system works"),
+('000009', '000002', '000009', '2023-01-01', 0, "Generic Message to show the system works"),
+('000010', '000002', '000011', '2023-01-01', 0, "Generic Message to show the system works"),
+('000011', '000007', '000001', '2023-01-01', 0, "Generic Message to show the system works"),
+('000012', '000007', '000003', '2023-01-01', 0, "Generic Message to show the system works"),
+('000013', '000007', '000004', '2023-01-01', 0, "Generic Message to show the system works"),
+('000014', '000007', '000005', '2023-01-01', 0, "Generic Message to show the system works"),
+('000015', '000007', '000006', '2023-01-01', 0, "Generic Message to show the system works"),
+('000016', '000007', '000010', '2023-01-01', 0, "Generic Message to show the system works");
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `schedule`
+--
+
+DROP TABLE IF EXISTS `schedule`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schedule` (
+  `employeeID` char(20) NOT NULL,
+  `startDate` date NOT NULL,
+  `startTime` time NOT NULL,
+  `endDate` date NOT NULL,
+  `endTime` time NOT NULL,
+  PRIMARY KEY (`employeeID`,`startDate`,`startTime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schedule`
+--
+
+LOCK TABLES `schedule` WRITE;
+/*!40000 ALTER TABLE `schedule` DISABLE KEYS */;
+INSERT INTO `schedule` VALUES 
+('000001','2023-11-27', '08:00:00','2023-11-27', '12:00:00'),
+('000001','2023-11-27', '14:00:00','2023-11-27', '20:00:00'),
+('000001','2023-11-28', '06:00:00','2023-11-28', '10:00:00'),
+('000001','2023-11-28', '11:00:00','2023-11-28', '15:00:00'),
+('000001','2023-11-28', '16:00:00','2023-11-28', '18:00:00'),
+('000001','2023-11-29', '08:30:00','2023-11-29', '19:30:00'),
+('000002','2023-11-25', '10:00:00','2023-11-25', '16:00:00');
+/*!40000 ALTER TABLE `schedule` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-03 15:23:40
---
+++ emps_UI_CLEAN/src/components/menu_four_Schedule_Builder/ScheduleBuilder.js
@@ -0,0 +1,568 @@
+import React, {useState, useEffect, Fragment} from 'react';
+import API from '../../API_Interface/API_Interface'
+
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'; 
+import { Height, SwitchAccessShortcutAddOutlined } from '@mui/icons-material';
+
+
+const ScheduleTableAttributes = [
+    {
+        title: 'Time',
+        align: 'left'
+    },
+    {
+        title: 'Monday',
+        align: 'left'
+    },
+    {
+        title: 'Tuesday',
+        align: 'left'
+    },
+    {
+        title: 'Wednsday',
+        align: 'left'
+    },
+    {   
+        title: 'Thursday',
+        alight: 'left'
+    },
+    {
+        title: 'Friday',
+        alight: 'left'
+    },
+    {
+        title: 'Saturday',
+        alight: 'left'
+    },
+    {
+        title: 'Sunday',
+        align: 'left'
+    }
+];
+
+//Finding the Date SUCKS
+function findMonday(date) {
+    let dateMonday = 0;
+
+    var dayName = date.slice(0, 3);
+    var day = date.slice(8,10);
+    var monthName = date.slice(4, 7); //January is 0!
+    var month = 0
+    var year = date.slice(11);
+
+    //Day Math
+    if (dayName === "Mon") {
+        dateMonday = day;
+    }
+    else {
+        if (dayName === "Tue") {
+            dateMonday = day - 1;
+        }
+        else if (dayName === "Wed") {
+            dateMonday = day - 2;
+        }
+        else if (dayName === "Thu") {
+            dateMonday = day - 3;
+        }
+        else if (dayName === "Fri") {
+            dateMonday = day - 4;
+        }
+        else if (dayName === "Sat") {
+            dateMonday = day - 5;
+        }
+        else if (dayName === "Sun") {
+            dateMonday = day - 6;
+        }
+    }
+
+    //Conver the month name into an integer
+    if (monthName === "Jan") {
+        month = 0;
+    }
+    else if (monthName === "Feb") {
+        month = 1;
+    }
+    else if (monthName === "Mar") {
+        month = 2;
+    }
+    else if (monthName === "Apr") {
+        month = 3;
+    }
+    else if (monthName === "May") {
+        month = 4;
+    }
+    else if (monthName === "Jun") {
+        month = 5;
+    }
+    else if (monthName === "Jul") {
+        month = 6;
+    }
+    else if (monthName === "Aug") {
+        month = 7;
+    }
+    else if (monthName === "Sep") {
+        month = 8;
+    }
+    else if (monthName === "Oct") {
+        month = 9;
+    }
+    else if (monthName === "Nov") {
+        month = 10;
+    }
+    else if (monthName === "Dec") {
+        month = 11;
+    }
+
+    //Month Math
+    if (dateMonday <= 0) 
+    {
+        if (month === 1) 
+        {
+            if (year % 4 === 0) 
+            {
+                dateMonday += 29;
+            }
+            else 
+            {
+                dateMonday += 28;
+            }
+        }
+        else if (month === 3 || month === 5 || month === 8 || month === 10) 
+        {
+            dateMonday += 30;
+        }
+        else 
+        {
+            dateMonday += 31;
+        }
+    }
+    
+    return dateMonday;
+}
+
+function weeklyViewAssembler(schedule, date) {
+    const currDay = findMonday(date);
+    let array = [];
+    let subArray = [];
+    let hours = 0;
+    let timeDisplay = "";
+
+    let mondayShift = dailyViewAssembler(schedule, currDay);
+    let tuesdayShift = dailyViewAssembler(schedule, currDay + 1);
+    let wednesdayShift = dailyViewAssembler(schedule, currDay + 2);
+    let thursdayShift = dailyViewAssembler(schedule, currDay + 3);
+    let fridayShift = dailyViewAssembler(schedule, currDay + 4);
+    let saturdayShift = dailyViewAssembler(schedule, currDay + 5);
+    let sundayShift = dailyViewAssembler(schedule, currDay + 6);
+
+    for (let i = 0; i < 48; i++) {
+        if (i % 2 === 0) {
+            hours = i/2; 
+            if (hours > 11.5) {
+                if (hours > 12) {
+                    timeDisplay = `${hours - 12}:00 PM`
+                }
+                else {
+                    timeDisplay = `${hours}:00 PM`
+                }
+            }
+            else {
+                timeDisplay = `${hours}:00 AM`
+            }
+
+            subArray =
+            {
+                hours: timeDisplay,
+                monday: mondayShift[i], 
+                tuesday: tuesdayShift[i],
+                wednesday: wednesdayShift[i],
+                thursday: thursdayShift[i],
+                friday: fridayShift[i],
+                saturday: saturdayShift[i],
+                sunday: sundayShift[i]
+            }
+        }
+        else {
+            hours = i/2 - 0.5;
+            if (hours > 11.5) {
+                if (hours > 12) {
+                    timeDisplay = `${hours - 12}:30 PM`
+                }
+                else {
+                    timeDisplay = `${hours}:30 PM`
+                }
+            }
+            else {
+                timeDisplay = `${hours}:30 AM`
+            }
+            subArray =
+            {
+                hours: timeDisplay,
+                monday: mondayShift[i], 
+                tuesday: tuesdayShift[i],
+                wednesday: wednesdayShift[i],
+                thursday: thursdayShift[i],
+                friday: fridayShift[i],
+                saturday: saturdayShift[i],
+                sunday: sundayShift[i]
+            }
+        }    
+        array.push(subArray);
+    }
+    return (array);
+}
+
+function dailyViewAssembler(schedule, currDay) {
+    let shifts = [];
+    let arrayToReturn = [];
+    let start;
+    let end;
+    let day;
+    for (let i = 0; i < schedule.length; i++) {
+        start = schedule[i].startTime;
+        end = schedule[i].endTime;
+        day = schedule[i].startDate.slice(8, 10);
+        if (day === String(currDay)) {
+            shifts.push(schedule[i]);
+        }
+    }
+
+    let startTime = 0;
+    let tickingTime = 0;
+    let hours = 0;
+    //console.log(shifts);
+    for (let i = 0, shiftItr = 0, inShift = 0, foundStart = 0; i < 48; i++) {
+        if (shiftItr >= shifts.length) {
+            arrayToReturn.push(0);
+        }
+        else if (inShift === 0){
+            if (shifts[shiftItr].startTime[shiftItr] === '0') {
+                startTime = shifts[shiftItr].startTime.slice(1, 5);
+            }
+            else {
+                startTime = shifts[shiftItr].startTime.slice(0, 5);
+            }
+            //console.log(startTime);
+
+            if (shifts[0].startTime.slice(3, 5) === 30) {
+                tickingTime = parseInt(shifts[shiftItr].endTime.slice(0, 3)) - parseInt(shifts[shiftItr].startTime.slice(0, 3)) + 0.5;
+            }
+            else {
+                tickingTime = parseInt(shifts[shiftItr].endTime.slice(0, 3)) - parseInt(shifts[shiftItr].startTime.slice(0, 3));
+            }
+            //console.log(tickingTime);
+            inShift = 1;
+            foundStart = 0;
+
+            if (i % 2 === 0) {
+                hours = i/2; 
+                if (startTime === `${hours}:00` && foundStart === 0) {
+                    foundStart = 1;
+                    arrayToReturn.push(1);
+                }
+                else {
+                    arrayToReturn.push(0);
+                }
+            }
+            else {
+                hours = i/2 - 0.5;
+                if (startTime === `${hours}:30` && foundStart === 0) {
+                    foundStart = 1;
+                    arrayToReturn.push(1);
+                }
+                else {
+                    arrayToReturn.push(0);
+                }
+            }
+        }
+        else if (foundStart === 1) {
+            if (tickingTime > 0) {
+                arrayToReturn.push(1);
+                tickingTime -= 0.5;
+            }
+            else {
+                arrayToReturn.push(0);
+                shiftItr++;
+                foundStart = 0;
+                inShift = 0;
+            }
+        }
+        else {
+            if (i % 2 === 0) {
+                hours = i/2; 
+                if (startTime === `${hours}:00` && foundStart === 0) {
+                    foundStart = 1;
+                    arrayToReturn.push(1);
+                }
+                else {
+                    arrayToReturn.push(0);
+                }
+            }
+            else {
+                hours = i/2 - 0.5;
+                if (startTime === `${hours}:30` && foundStart === 0) {
+                    foundStart = 1;
+                    arrayToReturn.push(1);
+                }
+                else {
+                    arrayToReturn.push(0);
+                }
+            }
+        }
+    }
+
+    //console.log(arrayToReturn);
+
+    return (arrayToReturn);
+}
+
+function colorHelper(value) {
+    if (value === 1) {
+        return 'lightblue';
+    }
+    else {
+        return 'white';
+    }
+}
+
+function ScheduleBox(value, key, swapColors, day, schedule, setSchedule) {
+    const [color, setColor] = useState(colorHelper(value));
+    return (
+        <Fragment>
+            <TableCell 
+                align="left" 
+                bgcolor={color}
+                onClick={() => 
+                    {
+                        swapColors(value, key, day, schedule, setSchedule, setColor);
+                    }
+                }
+                >
+                
+            </TableCell>
+        </Fragment>
+    )
+}
+
+async function getSchedule(employeeID, currentDate, setSchedule) {
+    
+    const api = new API();
+
+    const routesJSONString = await api.schedulesWithEmployeeID(employeeID);
+    //console.log(`messages from the DB ${JSON.stringify(routesJSONString)}`);
+    setSchedule(weeklyViewAssembler(routesJSONString.data, currentDate));
+}
+
+function swapColors(val, rowLoc, weekday, schedule, setSchedule, setColor) {
+
+    //Get the column to change
+    let row = schedule[rowLoc];
+    if (weekday === "Mon") {
+        if (val === 0) {
+            row.monday = 1;
+            setColor('lightblue')
+        } 
+        else {
+            row.monday = 0;
+            setColor('white')
+        } 
+    }
+    else if (weekday === "Tue") {
+        if (val === 0) {
+            row.tuesday = 1;
+            setColor('lightblue')
+        } 
+        else {
+            row.tuesday = 0;
+            setColor('white')
+        }
+    }
+    else if (weekday === "Wed") {
+        if (val === 0) {
+            row.wednesday = 1;
+            setColor('lightblue')
+        } 
+        else {
+            row.wednesday = 0;
+            setColor('white')
+        }
+    }
+    else if (weekday === "Thu") {
+        if (val === 0) {
+            row.thursday = 1;
+            setColor('lightblue')
+        } 
+        else {
+            row.thursday = 0;
+            setColor('white')
+        }
+    }
+    else if (weekday === "Fri") {
+        if (val === 0) {
+            row.friday = 1;
+            setColor('lightblue')
+        } 
+        else {
+            row.friday = 0;
+            setColor('white')
+        }
+    }
+    else if (weekday === "Sat") {
+        if (val === 0) {
+            row.saturday = 1;
+            setColor('lightblue')
+        } 
+        else {
+            row.saturday = 0;
+            setColor('white')
+        }
+    }
+    else if (weekday === "Sun") {
+        if (val === 0) {
+            row.sunday = 1;
+            setColor('lightblue')
+        } 
+        else {
+            row.sunday = 0;
+            setColor('white')
+        }
+    }
+
+    setSchedule(schedule);
+
+    //console.log(row);
+    //console.log(`${val} | Row: ${row} | Day: ${weekday}`);
+}
+
+export default function MessageTable(props) {
+
+    const currentDate = new Date().toDateString();
+    //console.log(currentDate.slice(4));
+    const [schedule, setSchedule] = useState([]);
+    const [selectedEmployeeID, setSelectedEmployeeID] = useState('000000');    
+
+    const handleEmployeeID = event => {
+        setSelectedEmployeeID(event.target.value);
+    };
+
+    useEffect(() => {
+        const api = new API();
+
+        async function getSchedule() {
+                const routesJSONString = await api.schedulesWithEmployeeID(selectedEmployeeID);
+                //console.log(`messages from the DB ${JSON.stringify(routesJSONString)}`);
+                setSchedule(weeklyViewAssembler(routesJSONString.data, currentDate));
+        }
+
+        getSchedule();
+    }, []);
+
+    const retrieveEmployeeSchedule = event => {
+        getSchedule(selectedEmployeeID, currentDate, setSchedule)
+    }; 
+
+    const TRow = (props) => {
+        const routeObject = props.routeObject;
+        //console.log(routeObject);
+        const key = props.val;
+        const swapColors = props.swapColors;
+
+        //TODO: Update the schedule so every :30 increment of time is updated.
+        return (
+        <Fragment>
+            <TableRow sx={{MaxHeight:'100%', MaxWidth:'100%'}}>
+                <TableCell align="left" bgcolor='f0f0f0'> {routeObject.hours} </TableCell> 
+                {
+                    ScheduleBox(routeObject.monday, key, swapColors, "Mon", schedule, setSchedule)
+                }
+                {
+                    ScheduleBox(routeObject.tuesday, key, swapColors, "Tue", schedule, setSchedule)
+                }
+                {
+                    ScheduleBox(routeObject.wednesday, key, swapColors, "Wed", schedule, setSchedule)
+                }
+                {
+                    ScheduleBox(routeObject.thursday, key, swapColors, "Thu", schedule, setSchedule)
+                }
+                {
+                    ScheduleBox(routeObject.friday, key, swapColors, "Fri", schedule, setSchedule)
+                }
+                {
+                    ScheduleBox(routeObject.saturday, key, swapColors,"Sat", schedule, setSchedule)
+                }
+                {
+                    ScheduleBox(routeObject.sunday, key, swapColors, "Sun", schedule, setSchedule)
+                }
+                
+            </TableRow>
+        </Fragment>
+        );
+    }
+
+    return <Fragment>
+        <Box display="flex" justifyContent="center" mt={3}>
+            <TextField
+                label="Employee ID"
+                value={selectedEmployeeID}
+                onChange = {handleEmployeeID}
+            />
+        </Box>
+
+        <Box display="flex" justifyContent="center" alignItems="center" width="100%" mt={2}>
+           <Button
+                    variant="outlined"
+                    size="medium"
+                    onClick={() => {retrieveEmployeeSchedule()}}
+                >Proceed</Button>
+           </Box>
+        {
+            schedule.length > 0 &&
+                <TableContainer component={Paper} sx = {{my:5}}>
+                    <Table sx={{maxWidth:'fix-content', maxHeight:'fit-content'}} aria-label="schedule table">
+                        <TableHead>
+                            <TableRow>
+                                {
+                                    ScheduleTableAttributes.map((attr, idx) =>
+                                        <TableCell  key={idx}
+                                                    align={attr.align}
+                                                    bgcolor='aa3f0ffa0'>
+                                                    {attr.title}
+                                        </TableCell>)
+                                }
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                schedule.map((schedule, idx) => (
+                                    <TRow
+                                        swapColors={swapColors} 
+                                        routeObject={schedule} 
+                                        val={idx}
+                                    />
+                                ))
+                            }
+
+                            
+
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+        }
+    </Fragment>
+}
