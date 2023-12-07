59b9de28ef0bd579a82ae4937d61b3afb00389fa
Alex Hicks <89438002+hicksale@users.noreply.github.com>
2023-12-07 13:45:12 -0800

    Update ScheduleBuilder.js

--- emps_UI_CLEAN/src/components/menu_four_Schedule_Builder/ScheduleBuilder.js
+++ emps_UI_CLEAN/src/components/menu_four_Schedule_Builder/ScheduleBuilder.js
@@ -72,7 +72,7 @@ function findMonday(date) {
 
     var dayName = date.slice(0, 3);
     var day = date.slice(8,10);
-    var monthName = date.slice(4, 7); //January is 0!
+    var monthName = date.slice(4, 7); //January is 1!
     var month = 0
     var year = date.slice(11);
     var maxDayInMonth = 0;
@@ -104,47 +104,47 @@ function findMonday(date) {
 
     //Conver the month name into an integer
     if (monthName === "Jan") {
-        month = 0;
-    }
-    else if (monthName === "Feb") {
         month = 1;
     }
-    else if (monthName === "Mar") {
+    else if (monthName === "Feb") {
         month = 2;
     }
-    else if (monthName === "Apr") {
+    else if (monthName === "Mar") {
         month = 3;
     }
-    else if (monthName === "May") {
+    else if (monthName === "Apr") {
         month = 4;
     }
-    else if (monthName === "Jun") {
+    else if (monthName === "May") {
         month = 5;
     }
-    else if (monthName === "Jul") {
+    else if (monthName === "Jun") {
         month = 6;
     }
-    else if (monthName === "Aug") {
+    else if (monthName === "Jul") {
         month = 7;
     }
-    else if (monthName === "Sep") {
+    else if (monthName === "Aug") {
         month = 8;
     }
-    else if (monthName === "Oct") {
+    else if (monthName === "Sep") {
         month = 9;
     }
-    else if (monthName === "Nov") {
+    else if (monthName === "Oct") {
         month = 10;
     }
-    else if (monthName === "Dec") {
+    else if (monthName === "Nov") {
         month = 11;
+    }
+    else if (monthName === "Dec") {
+        month = 12;
     }
 
     //Month Math
     if (dateMonday <= 0) 
     {
         month--;
-        if (month === 1) 
+        if (month === 2) 
         {
             if (year % 4 === 0) 
             {
@@ -157,7 +157,7 @@ function findMonday(date) {
                 dateMonday += 28;
             }
         }
-        else if (month === 3 || month === 5 || month === 8 || month === 10) 
+        else if (month === 4 || month === 6 || month === 9 || month === 11) 
         {
             maxDayInMonth = 30;
             dateMonday += 30;
@@ -168,15 +168,36 @@ function findMonday(date) {
             dateMonday += 31;
         }
     }
+    else {
+        if (month === 2) 
+        {
+            if (year % 4 === 0) 
+            {
+                maxDayInMonth = 29;
+            }
+            else 
+            {
+                maxDayInMonth = 28;
+            }
+        }
+        else if (month === 4 || month === 6 || month === 9 || month === 11) 
+        {
+            maxDayInMonth = 30;
+        }
+        else 
+        {
+            maxDayInMonth = 31;
+        }
+    }
 
-    return [dateMonday, maxDayInMonth];
+    return [dateMonday, month, maxDayInMonth];
 }
 
 function weeklyViewAssembler(schedule, date) {
     let value = findMonday(date);
     var currDay = value[0];
     var offset = 0;
-    const maxDaysInMonth = value[1];
+    const maxDaysInMonth = value[2];
 
     console.log(`${maxDaysInMonth} | ${currDay}`)
     let array = [];
@@ -187,7 +208,7 @@ function weeklyViewAssembler(schedule, date) {
     let mondayShift = dailyViewAssembler(schedule, currDay + offset);
 
     offset++;
-    if (currDay > maxDaysInMonth) {
+    if (currDay + offset > maxDaysInMonth) {
         currDay = 1;
         offset = 0;
         //console.log(`Hit Monday | ${currDay}`)
@@ -402,6 +423,19 @@ function dailyViewAssembler(schedule, currDay) {
     return (arrayToReturn);
 }
 
+function dataParserDropTable(message) {
+    let array = []
+    for (let i = 1; i < message.length; i++) {
+        array.push({
+            name: message[i].firstName + ` ` + message[i].lastName,
+            employeeID: message[i].employeeID
+        })
+    }
+
+    console.log(array);
+    return array;
+}
+
 function colorHelper(value, val2) {
     if (value === 0) {
         if (val2 === 0) {
@@ -449,9 +483,134 @@ async function getSchedule(employeeID, currentDate, setSchedule) {
     setSchedule(weeklyViewAssembler(routesJSONString.data, currentDate));
 }
 
-//TODO Update for updating the schedule
-function updateSchedule(schedule) {
-    console.log(schedule)
+function updateSchedule(schedule, date, employeeID) {
+    let value = findMonday(date);
+    const maxDaysInMonth = value[2];
+    let month = value[1];
+    var currDay = value[0];
+    let year = parseInt(date.slice(11));
+    var offset = 0;
+
+    //Break up schedule into 7 different arrays
+    let superArray = [];
+    let arrayMon = [];
+    let arrayTue = [];
+    let arrayWed = [];
+    let arrayThu = [];
+    let arrayFri = [];
+    let arraySat = [];
+    let arraySun = [];
+
+    for (let i = 0; i < schedule.length; i++) {
+        arrayMon.push(schedule[i].monday);
+        arrayTue.push(schedule[i].tuesday);
+        arrayWed.push(schedule[i].wednesday);
+        arrayThu.push(schedule[i].thursday);
+        arrayFri.push(schedule[i].friday);
+        arraySat.push(schedule[i].saturday);
+        arraySun.push(schedule[i].sunday);
+    }
+    superArray.push(arrayMon, arrayTue, arrayWed, arrayThu, arrayFri, arraySat, arraySun);
+
+    for (let i = 0; i < 7; i++) {
+        //Pass date through to addAndRemoveSchedule
+        if ((currDay + offset) < 10) {
+            addAndRemoveSchedule(superArray[i], `${year}-${month}-0${currDay + offset}`, employeeID)
+        }
+        else {
+            addAndRemoveSchedule(superArray[i], `${year}-${month}-${currDay + offset}`, employeeID)
+        }
+
+        //Prepare next date here
+        offset++;
+        if ((currDay + offset) > maxDaysInMonth) {
+            currDay = 1;
+            month++;
+            offset = 0;
+            if (month > 12) {
+                year++;
+            }
+        }
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+//TODO: Perform API updates
+function addAndRemoveSchedule(dayArray, day, employeeID) {
+    let startTimes = [];
+    let endTimes = [];
+
+    //Find the start and end times
+    for (let i = 0, recTime = 0; i < dayArray.length; i++) {
+        if (dayArray[i] === 1 && recTime === 0) {
+            if (i % 2 === 0) {
+                startTimes.push(`${i / 2}:00:00`);
+            }
+            else {
+                startTimes.push(`${(i / 2) - 0.5}:30:00`);
+            }
+            recTime = 1;
+            
+            //Check if the start and endtime are the same block of time
+            if (i + 1 >= dayArray.length && dayArray[i] === 1 && recTime === 1) {
+                if (i % 2 === 0) {
+                    endTimes.push(`${(i / 2)}:00:00`);
+                }
+                else {
+                    endTimes.push(`${(i / 2) - 0.5}:30:00`);
+                }
+                recTime = 0;
+            }
+        }
+        else if (dayArray[i] === 0 && recTime === 1) {
+            if (i % 2 === 0) {
+                endTimes.push(`${(i / 2) - 1}:30:00`);
+            }
+            else {
+                endTimes.push(`${(i / 2) - 0.5}:00:00`);
+            }
+            recTime = 0;
+        }
+        else if (i + 1 >= dayArray.length && dayArray[i] === 1 && recTime === 1) {
+            if (i % 2 === 0) {
+                endTimes.push(`${(i / 2)}:00:00`);
+            }
+            else {
+                endTimes.push(`${(i / 2) - 0.5}:30:00`);
+            }
+            recTime = 0;
+        } 
+    }
+
+    //Perform global day delete and then updates per startTime and endTime
+    remove(day, employeeID);
+    sleep(5000) //Wait for 2 seconds
+    for (let i = 0; i < startTimes.length; i++) {
+        add(day, employeeID, startTimes[i], endTimes[i])
+    }
+    
+    /*
+    console.log(dayArray);
+    console.log(day);
+    console.log(startTimes);
+    console.log(endTimes);
+    console.log(employeeID);
+    */
+}
+
+async function remove(day, employeeID) {
+    const api = new API();
+
+    await api.deleteSchedule(day, employeeID);
+}
+
+async function add(day, employeeID, startTime, endTime) {
+    const api = new API();
+
+    await api.addSchedule(day, employeeID, startTime, day, endTime);
 }
 
 function swapColors(val, val2, rowLoc, weekday, schedule, setSchedule, setColor) {
@@ -632,11 +791,25 @@ export default function MessageTable(props) {
 
     const currentDate = new Date().toDateString();
     const [schedule, setSchedule] = useState([]);
-    const [selectedEmployeeID, setSelectedEmployeeID] = useState('000000');    
+    const [selectedEmployeeID, setSelectedEmployeeID] = useState('000001');  
+    const [dropDownTable, setdropDownTable] = useState([]); 
 
-    const handleEmployeeID = event => {
-        setSelectedEmployeeID(event.target.value);
-    };
+    const createSelectItems = () => {
+        let items = [];
+        //console.log(dropDownTable.length)
+        for (let i = 0; i < dropDownTable.length; i++) {             
+             items.push(<option key={i} value={dropDownTable[i].employeeID}>{dropDownTable[i].name}</option>);   
+             //here I will be creating my options dynamically based on
+             //what props are currently passed to the parent component
+        }
+        return items;
+    }  
+
+    const onDropdownSelected = (e) => {
+        setSelectedEmployeeID(e.target.value);
+        //console.log("THE VAL", receiverID);
+        //here you will see the current selected value of the select input
+    }
 
     useEffect(() => {
         const api = new API();
@@ -648,6 +821,14 @@ export default function MessageTable(props) {
         }
 
         getSchedule();
+
+        async function getDropDown() {
+            const routesJSONStringTwo = await api.allEmployees();
+            //console.log(`messages from the DB ${JSON.stringify(routesJSONString)}`);
+            setdropDownTable(dataParserDropTable(routesJSONStringTwo.data));
+        }
+        
+        getDropDown();
     }, []);
 
     const retrieveEmployeeSchedule = event => {
@@ -680,7 +861,7 @@ export default function MessageTable(props) {
         return (
         <Fragment>
             <TableRow sx={{MaxHeight:'100%', MaxWidth:'100%'}}>
-                <TableCell align="left" bgcolor={colorAlternator2(colorVal)}> {routeObject.hours} </TableCell> 
+                <TableCell align="left" bgcolor={colorAlternator2(colorVal)} sx={{fontSize: 'large'}}> {routeObject.hours} </TableCell> 
                 {
                     ScheduleBox(routeObject.monday, 0, key, swapColors, "Mon", schedule, setSchedule)
                 }
@@ -710,7 +891,7 @@ export default function MessageTable(props) {
 
     return <Fragment>
 
-        <Box display="flex" justifyContent="center" mt={2} gap={0}> 
+        <Box display="flex" justifyContent="center" mt={2} gap={10}> 
             <Box></Box>
             <Box display="flex" justifyContent="right" alignItems="center" width="100%">
                 <Button
@@ -722,19 +903,15 @@ export default function MessageTable(props) {
                 </Button>
            </Box>
 
-           <Box display="flex" justifyContent="center" width="100%">
-                <TextField
-                    label="Employee ID"
-                    value={selectedEmployeeID}
-                    onChange = {handleEmployeeID}
-                />
-            </Box>
+           <select onChange={onDropdownSelected}>
+                {createSelectItems()}
+            </select>
            
            <Box display="flex" justifyContent="left" alignItems="center" width="100%">
                 <Button
                     variant="outlined"
                     size="medium"
-                    onClick={() => {updateSchedule(schedule)}}
+                    onClick={() => {updateSchedule(schedule, currentDate, selectedEmployeeID)}}
                 >
                     Update Schedule
                 </Button>
@@ -743,14 +920,15 @@ export default function MessageTable(props) {
         {
             schedule.length > 0 &&
                 <TableContainer component={Paper} sx = {{my:5}}>
-                    <Table sx={{maxWidth:'fix-content', maxHeight:'fit-content'}} aria-label="schedule table">
+                    <Table sx={{maxWidth:'fix-content', maxeight:'fit-content'}} aria-label="schedule table" >
                         <TableHead>
                             <TableRow>
                                 {
                                     ScheduleTableAttributes.map((attr, idx) =>
                                         <TableCell  key={idx}
                                                     align={attr.align}
-                                                    bgcolor={attr.color}>
+                                                    bgcolor={attr.color}
+                                                    sx={{fontSize: 'large'}}>
                                                     {attr.title}
                                         </TableCell>)
                                 }
