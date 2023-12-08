d2f7a941e39bc5ef22abc10f3b28900565bc5e94
Alex Hicks <89438002+hicksale@users.noreply.github.com>
2023-12-07 16:56:39 -0800

    Update mySQLconnect.js

--- emps_API_CLEAN/database/mySQLconnect.js
+++ emps_API_CLEAN/database/mySQLconnect.js
@@ -17,8 +17,8 @@ var connection = mysql.createConnection({
 
     host: 'localhost',
     port: 3306,
-    user: 'marcWeg',
-    password: 'pencils',
+    user: 'username_here',
+    password: 'password_here',
     database: 'emps_services'
 });
 
